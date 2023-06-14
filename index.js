aaf8d9dd4eef633003645045d0be930fbc3bccd7
Brian Kibet <kibetsbrian@gmail.com>
2023-06-14 17:41:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code in this file!
+const currentUser = "Grace Hopper";
+
+var welcomeMessage = "Welcome to Flatbook, "  + currentUser + "!";
+
+// var welcomeMessage = "welcome to Flatbook, "
+
+const  excitedWelcomeMessage = `Welcome to Flatbook, ${currentUser} + !`.toUpperCase()  ;
+
+const  shortGreeting = "Welcome, " + currentUser.slice(0,1) + "!";
